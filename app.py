56c310c0201917feea62ef0ebf7e15022cd71895
ChetanKothawale <140860847+ChetanKothawale@users.noreply.github.com>
2025-04-11 06:32:45 +0530

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 import imageio
 import torch
 from filters import butterworth_lowpass_filter, anisotropic_diffusion, median_filter, bilateral_filter_color, gaussian_filter
-from denoise_model import load_image, load_dncnn_model, denoise_image
+from DnCNN import load_image, load_dncnn_model, denoise_image
 
 st.title("Image Filtering & Denoising with Streamlit")
 
